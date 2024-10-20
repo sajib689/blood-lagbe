248bc7b492feacf82ddc3da74ae5dcd2fedf04cc
codeXfarm <codexfarm24@gmail.com>
2024-10-20 15:53:31 +0600

    Organize file and folder

---
+++ src/app/(landing-page)/about/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/container";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <Container>
+      <div>AboutPage</div>
+    </Container>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+  );
+}
