f65a4bdd489795511ebe56fcdda265cafee57b23
git12nazneen <lipinazneen65@gmail.com>
2024-10-25 10:54:14 -0700

    lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ const LandingPage = () => {
     <div>
       <h1 className="bg-gray-100 h-screen items-center text-center">Banner test</h1>
       <p>sabbir added text</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident quam tempora nemo quod libero dolor vero maiores architecto rem nisi.</p>
     </div>
   )
 };
