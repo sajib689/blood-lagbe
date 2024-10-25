94c0ac5d6e6bf9e7b23215b672045b060cae9cc4
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-10-25 23:19:41 +0600

    [sabbir]added nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-      <h1 className="bg-gray-100 h-screen items-center text-center">Banner test</h1>
+      <h1 className="bg-gray-100 items-center text-center">Banner test</h1>
       <p>sabbir added text</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident quam tempora nemo quod libero dolor vero maiores architecto rem nisi.</p>
+      <i>hello hello hello jonogon , tumi roshi diya mon</i>
     </div>
   )
 };
