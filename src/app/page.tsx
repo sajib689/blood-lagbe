755451e022032e0424dd188d5f79d2f360a65c23
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-10-25 22:40:43 +0600

    [sabbir]for test git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-  <div>
-    <h1 className="bg-gray-100 h-screen items-center text-center">Banner test</h1>
-  </div>
-)};
+    <div>
+      <h1 className="bg-gray-100 h-screen items-center text-center">Banner test</h1>
+      <p>sabbir added text</p>
+    </div>
+  )
+};
 
 export default LandingPage;
 
