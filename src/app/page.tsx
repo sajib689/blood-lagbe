ace758af5595d44fb94c1a61609921d1689d73c5
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-10-25 23:29:45 +0600

    [sabbir]added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ const LandingPage = () => {
       <p>sabbir added text</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident quam tempora nemo quod libero dolor vero maiores architecto rem nisi.</p>
       <i>hello hello hello jonogon , tumi roshi diya mon</i>
+      <p>sabbir give it again</p>
     </div>
   )
 };
